eeb3d70543776b435e75c4630051e08d0db52935
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-10-02 11:54:22 -0400

    debug data reader
    Co-authored-by: Michael Huang <michaelyhuang23@users.noreply.github.com>

--- data_reading.py
+++ data_reading.py
@@ -3,12 +3,12 @@ import os
 import cv2
 import torch
 from torch.utils.data import Dataset
+from torchvision.datasets.coco import CocoDetection
 
-class WindowDataset(Dataset):
+class WindowDataset(CocoDetection):
     def __init__(self, labels_folder = 'data/labels/', images_folder = 'data/images/'):
         super(Dataset, self).__init__()
-        self.images, self.labels = [], []
-
+        self.data = []
         image_width = 128
         image_height = 96
 
@@ -17,27 +17,25 @@ class WindowDataset(Dataset):
                 for filename in os.listdir(f"{labels_folder}{folder}"):
                     if filename.endswith("YOLO"):
                         for _file in os.listdir(f"{labels_folder}{folder}/{filename}"):
+                            self.data.append([None,None])
                             with open(f"{labels_folder}{folder}/{filename}/{_file}") as f:
-                                lines = []
+                                boxes = []
+                                labels = []
                                 for line in f:
                                     line = line.split()
                                     label, rest = int(line[0]), line[1:]
                                     xmin, ymin, w, h = map(float, rest)
                                     xmin, ymin = int(xmin*image_width), int(ymin*image_height)
                                     w, h = int(w*image_width), int(h*image_height)
-                                    lines.append([label, xmin, ymin, w+xmin, h+ymin])
-                                lines = np.array(lines)
-                                self.labels.append(torch.tensor(lines))
+                                    boxes.append(torch.tensor([xmin, ymin, w+xmin, h+ymin]))
+                                    labels.append(label)
+                                self.data[-1][1] = {'boxes':torch.stack(boxes).float(), 'labels':torch.tensor(labels).long()}
                             image = torch.tensor(cv2.cvtColor(cv2.imread(f"{images_folder}/{folder}/{_file[:-3]}jpg", cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB))
                             image = image.permute(2,0,1).double()
-                            print(image.shape)
-                            self.images.append(image)
+                            self.data[-1][0] = image
         
     def __getitem__(self, idx):
-        boxes = self.labels[idx][:,1:]
-        labels = self.labels[idx][:,0]
-        print(boxes.shape)
-        return (self.images[idx:idx+1], [{'boxes':boxes, 'labels':labels}])
+        return tuple(self.data[idx])
     
     def __len__(self):
-        return len(self.labels)
+        return len(self.data)
